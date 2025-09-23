7cb4a7e0e67a7c245173bc809ae895eaf762fa5c
Jan Funke <funkej@janelia.hhmi.org>
2025-09-23 11:36:06 -0400

    Add Model to keep all simulation parameters and sub-models

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flax import nnx
+from cell import Cell
 import jax
+import jax.numpy as jnp
 
 
 class ReactModel(nnx.Module):
@@ -68,3 +70,41 @@ class SplitModel(nnx.Module):
     def sample(self, split_probs):
         uniform = jax.random.uniform(self.rngs.split_probs(), shape=split_probs.shape)
         return split_probs > uniform
+
+
+class Model(nnx.Module):
+    def __init__(
+        self, max_num_cells, cell_state_dims, react_model, split_model, *, rngs
+    ):
+        self.max_num_cells = max_num_cells
+        self.cell_state_dims = cell_state_dims
+        self.initial_cell_state = nnx.Param(
+            rngs.uniform((max_num_cells, cell_state_dims))
+        )
+        self.react_model = react_model
+        self.split_model = split_model
+
+    def initialize_cells(self):
+        initial_cells = Cell(
+            position=jnp.zeros((self.max_num_cells, 3)),
+            state=self.initial_cell_state.value,
+            # initially, only one cell is active
+            parent=(-jnp.ones((self.max_num_cells,), dtype=jnp.int16)).at[0].set(0),
+            p_split=self.split_model(self.initial_cell_state),
+            split=None,
+        )
+        initial_cells = initial_cells.replace(
+            split=self.split_model.sample(initial_cells.p_split)
+        )
+        return initial_cells
+
+    def split(self):
+        return nnx.split(self, nnx.Param, ...)
+
+    @classmethod
+    def create(cls, definition, params, state):
+        return nnx.merge(definition, params, state)
+
+    def update_params(self, params):
+        model_def, _, state = nnx.split(self, nnx.Param, ...)
+        return nnx.merge(model_def, params, state)
