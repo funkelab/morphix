58702667bc603476c64ba1801102fd3b95b4ce0d
Jan Funke <funkej@janelia.hhmi.org>
2025-09-23 11:45:59 -0400

    Rewrite train script to use Model and clean parameter/state separation
    Right now, the outcome of learning depends on calls to run_simulation,
    which prints a lot and slows down training. It does not seem to be
    side-effect free though. Removing those lines seems lead to
    deterministic behavior, I believe there is something fishy about the RNG
    states.

--- train.py
+++ train.py
@@ -8,135 +8,106 @@
 # ]
 # ///
 from flax import nnx
-from models import ReactModel, SplitModel
-from cell import Cell
+from models import ReactModel, SplitModel, Model
 from simulation import simulate, simulation_step
 from loss import trajectory_loss
 from functools import partial
 from tqdm import tqdm
 from utils import print_cells
 import jax
-import jax.numpy as jnp
 import optax
 
 
 def simulation_loss(
-    initial_cells: Cell,
-    model_def: nnx.GraphDef,
-    model_state: nnx.State,
-    max_num_cells: int,
+    model_def,
+    params,
+    state,
     num_timesteps: int,
 ) -> float:
     # run the simulation
-    trajectory, model_state = simulate(
-        initial_cells, model_def, model_state, num_timesteps
-    )
+    trajectory = simulate(model_def, params, state, num_timesteps)
 
     # compute the loss
     return trajectory_loss(trajectory)
 
 
-@partial(jax.jit, static_argnames=("max_num_cells", "num_timesteps", "optimizer"))
+@partial(jax.jit, static_argnames=("num_timesteps", "optimizer"))
 def train_step(
-    cells,
     model_def,
-    model_state,
-    model_params,
-    max_num_cells,
+    params,
+    state,
     num_timesteps,
     optimizer,
     opt_state,
 ):
-    def loss_fn(learnable_params):
-        model_params, initial_cell_states = learnable_params
-        m = nnx.merge(model_def, model_state, model_params)
-        m_state = nnx.state(m)
-        c = cells.replace(state=initial_cell_states)
-        return simulation_loss(c, model_def, m_state, max_num_cells, num_timesteps)
+    def loss_fn(params):
+        return simulation_loss(model_def, params, state, num_timesteps)
 
-    loss, grad = jax.value_and_grad(loss_fn)((model_params, cells.state))
+    loss, grad = jax.value_and_grad(loss_fn)(params)
 
-    updates, opt_state = optimizer.update(grad, opt_state, (model_params, cells.state))
-    model_params, initial_cell_states = optax.apply_updates(
-        (model_params, cells.state), updates
-    )
+    updates, opt_state = optimizer.update(grad, opt_state, params)
+    params = optax.apply_updates(params, updates)
 
-    cells = cells.replace(state=initial_cell_states)
-
-    return loss, model_params, cells
+    return loss, params
 
 
-if __name__ == "__main__":
-    max_num_cells = 8
-    cell_state_dims = 32
-    num_timesteps = 20
-    num_iterations = 100_000
-    learning_rate = 1e-6
-    exploration_eps = 0.1
-
-    cells = Cell(
-        position=jnp.zeros((max_num_cells, 3)),
-        state=nnx.Rngs(1912).uniform((max_num_cells, cell_state_dims)),
-        # initially, only one cell is active
-        parent=(-jnp.ones((max_num_cells,), dtype=jnp.int16)).at[0].set(0),
-        p_split=jnp.ones((max_num_cells,)) * 1e-4,
-        split=jnp.zeros((max_num_cells,), dtype=jnp.bool),
-    )
-
-    rngs = nnx.Rngs(params=0, dropout=1, split_probs=3)
+def create_model(max_num_cells, cell_state_dims, exploration_eps):
+    rngs = nnx.Rngs(default=0, params=1, dropout=2, split_probs=3)
     react_model = ReactModel(cell_state_dims, cell_state_dims * 2, rngs=rngs)
     split_model = SplitModel(
         cell_state_dims, cell_state_dims * 2, eps=exploration_eps, rngs=rngs
     )
-    model_def, model_params, model_state = nnx.split(
-        (react_model, split_model), nnx.Param, ...
-    )
+    model = Model(max_num_cells, cell_state_dims, react_model, split_model, rngs=rngs)
+    return model
 
-    # we want to optimize both the model params and the inital cell state
-    learnable_params = (model_params, cells.state)
 
-    optimizer = optax.adamw(learning_rate=learning_rate)
-    opt_state = optimizer.init(learnable_params)
-
-    # print a few iterations (new variables to avoid overwriting the inital
-    # state)
-    model = nnx.merge(model_def, model_state, model_params)
-    m = nnx.state(model)
-    c = cells
-    for t in range(10):
-        print(f"{t=}:")
-        print_cells(c)
-        c, m = simulation_step(c, model_def, m, max_num_cells)
-
-    print(f"Initial model state {model_state}")
-    print(f"Initial model params {model_params}")
-
-    iteration_range = tqdm(range(num_iterations))
-    for i in iteration_range:
-        loss, model_params, cells = train_step(
-            cells,
-            model_def,
-            model_state,
-            model_params,
-            max_num_cells,
-            num_timesteps,
-            optimizer,
-            opt_state,
-        )
-        iteration_range.set_description(f"{loss=}")
-        if i % 1000 == 0:
-            print(f"iteration {i}: loss {loss}")
-
-    # print a few iterations:
-    split_model = SplitModel(cell_state_dims, cell_state_dims * 2, rngs=rngs)
-    model_def, _, model_state = nnx.split((react_model, split_model), nnx.Param, ...)
-    model = nnx.merge(model_def, model_state, model_params)
-    model_state = nnx.state(model)
-    print(f"Final model state {model_state}")
-    print(f"Final model params {model_params}")
-    for t in range(10):
+def run_simulation(model, num_timesteps):
+    model_def, params, state = model.split()
+    cells = model.initialize_cells()
+    # print(f"model state {state}")
+    # print(f"model params {params}")
+    print()
+    print()
+    for t in range(min(num_timesteps, 10)):
         print(f"{t=}:")
         print_cells(cells)
-        cells, model_state = simulation_step(
-            cells, model_def, model_state, max_num_cells
-        )
+        cells, model_state = simulation_step(cells, model_def, params, state)
+
+
+if __name__ == "__main__":
+    max_num_cells = 2
+    cell_state_dims = 64
+    num_timesteps = 8
+    num_iterations = 10_000
+    learning_rate = 1e-4
+    exploration_eps = 0.0
+
+    model = create_model(max_num_cells, cell_state_dims, exploration_eps)
+    cells = model.initialize_cells()
+
+    model_def, params, state = model.split()
+    optimizer = optax.adamw(learning_rate=learning_rate)
+    opt_state = optimizer.init(params)
+
+    run_simulation(model, num_timesteps)
+
+    try:
+        iteration_range = tqdm(range(num_iterations))
+        for i in iteration_range:
+            run_simulation(model, num_timesteps)
+            loss, params = train_step(
+                model_def,
+                params,
+                state,
+                num_timesteps,
+                optimizer,
+                opt_state,
+            )
+            iteration_range.set_description(f"{loss.item():.3f}")
+            model = model.update_params(params)
+    except KeyboardInterrupt:
+        print("Training stopped early.")
+        pass
+
+    for i in range(2):
+        run_simulation(model, num_timesteps)
