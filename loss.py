57c18aa2f6bb5c2dcefeede9d0733a2e951bee9e
Jan Funke <funkej@janelia.hhmi.org>
2025-09-23 11:35:10 -0400

    Compute trajectory loss per cell (and timestep)
    This is mainly to zero-out losses for inactive cells.

--- loss.py
+++ loss.py
@@ -6,14 +6,21 @@ from cell import Cell
 def reward(cells: Cell, t):
     """Compute the reward of a cell state at a given point in time."""
 
+    active = cells.parent >= 0
+
     # placeholder: reward as few cells as possible
-    num_active = (cells.parent >= 0).sum()
+    num_active = active.sum()
 
-    # one cell for t = 0, 1; then two cells
-    target = 1 + (t > 2)
+    # one cell until t=3; then two cells
+    target = 1 + (t > 3)
 
-    # reward is negative deviation from target
-    return -jnp.abs(num_active - target)
+    diff = num_active - target
+
+    # reward is negative deviation from target, but only for active cells
+    rewards = active * -jnp.abs(diff)
+
+    # (n,)
+    return rewards
 
 
 def trajectory_rewards(cells: Cell):
@@ -22,17 +29,37 @@ def trajectory_rewards(cells: Cell):
     return jax.vmap(reward)(cells, jnp.arange(num_timesteps))
 
 
-def trajectory_loss(cells: Cell):
+def trajectory_loss(cells: Cell, debug=False):
+    # (t, n)
     rewards = trajectory_rewards(cells)
 
     # compute log probabilities of each action taken
     # p_action: (t, n)
     p_action = cells.p_split * cells.split + (1.0 - cells.p_split) * (1 - cells.split)
-    # log_p_action: (t,)
-    log_p_action = jnp.log(p_action).sum(axis=1)
+    # log_p_action: (t, n)
+    log_p_action = jnp.log(p_action)
 
-    # compute "reward-to-go" for each timestep, i.e., sum of future rewards
-    # rewards_to_go: (t,)
-    rewards_to_go = jnp.cumsum(rewards[::-1])[::-1]
+    # compute "reward-to-go" for each timestep and cell, i.e., sum of future
+    # rewards rewards_to_go: (t, n)
+    rewards_to_go = jnp.cumsum(rewards[::-1], axis=0)[::-1]
 
-    return -(log_p_action * rewards_to_go).sum()
+    if debug:
+        jax.debug.print("rewards: {}", rewards)
+        jax.debug.print("split: {}", cells.split)
+        jax.debug.print("p_split: {}", cells.p_split)
+        jax.debug.print("p_action: {}", p_action)
+        jax.debug.print("log_p_action: {}", log_p_action)
+        jax.debug.print("rewards_to_go {}", rewards_to_go)
+
+    # zero-out rewards for inactive cells
+    active = cells.parent >= 0
+    rewards_to_go = rewards_to_go * active
+
+    # per-cell losses: (t, n)
+    losses = -log_p_action * rewards_to_go
+
+    if debug:
+        jax.debug.print("rewards_to_go * active {}", rewards_to_go)
+        jax.debug.print("per-cell losses: {}", losses)
+
+    return losses.sum()
