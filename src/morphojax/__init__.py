5ea03a58864ffe1205cf93a31c7aa8aaa8d7ab8b
Jan Funke <funkej@janelia.hhmi.org>
2025-10-02 14:02:32 -0400

    Move benchmark into separate folder and adjust to use package

--- src/morphojax/__init__.py
+++ src/morphojax/__init__.py
@@ -1,6 +1,13 @@
 from .models import create_model
+from .simulation import simulate, simulation_step
 from .train import train_step
 from .utils import print_simulation
 
-__all__ = ["create_model", "print_simulation", "train_step"]
+__all__ = [
+    "create_model",
+    "print_simulation",
+    "simulate",
+    "simulation_step",
+    "train_step",
+]
 __version__ = "0.0.1"
